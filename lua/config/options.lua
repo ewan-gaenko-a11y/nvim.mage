bd1aabe3854d009901656d7d13bc4826b6e3ba9d
ewan-gaenko-a11y <ewan.gaenko@yandex.ru>
2025-09-08 22:41:01 +0300

    add trouble plugin and system clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,4 @@
-vim.opt.number = true
+-- basics
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
@@ -6,49 +6,9 @@ vim.opt.smartindent = true
 vim.opt.relativenumber = true
 vim.opt.number = true
 
+-- disable some default bullshit
 vim.opt.wrap = false
 vim.opt.swapfile = false
--- -- Some servers have issues with backup files, see #649
--- vim.opt.backup = false
--- vim.opt.writebackup = false
---
--- -- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
--- -- delays and poor user experience
--- vim.opt.updatetime = 300
---
--- -- Always show the signcolumn, otherwise it would shift the text each time
--- -- diagnostics appeared/became resolved
--- vim.opt.signcolumn = "yes"
--- local keyset = vim.keymap.set
--- -- Autocomplete
--- function _G.check_back_space()
---     local col = vim.fn.col('.') - 1
---     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
--- end
--- -- Use K to show documentation in preview window
--- function _G.show_docs()
---     local cw = vim.fn.expand('<cword>')
---     if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
---         vim.api.nvim_command('h ' .. cw)
---     elseif vim.api.nvim_eval('coc#rpc#ready()') then
---         vim.fn.CocActionAsync('doHover')
---     else
---         vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
---     end
--- end
--- keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
--- -- Use Tab for select completion
--- local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
--- keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(2) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
--- keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(2) : "\<C-h>"]], opts)
--- -- Use <c-space> to trigger completion
--- keyset("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
--- -- GoTo code navigation
--- keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
--- keyset("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
--- keyset("n", "gi", "<Plug>(coc-implementation)", {silent = true})
--- keyset("n", "gr", "<Plug>(coc-references)", {silent = true})
---
--- -- Make <CR> to accept selected completion item or notify coc.nvim to format
--- -- <C-g>u breaks current undo, please make your own choice
--- keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+
+-- hehe system clipboard
+vim.api.nvim_set_option("clipboard", "unnamed")
